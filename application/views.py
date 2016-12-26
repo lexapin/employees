b01da577bee021cc552c272422dca551039911f8
Fomin Alexey <fomin_ai@ekra.ru>
2016-12-26 10:09:12 +0300

    zaebalsya
    Signed-off-by: Fomin Alexey <fomin_ai@ekra.ru>

--- application/views.py
+++ application/views.py
@@ -55,5 +55,5 @@ def index():
     user = "not imported"
   else:
     user = "password %s"%admin
-  returned_string = repr(current_user).encode('utf-8') + user
+  returned_string = repr(current_user).encode('utf-8') + user.encode('utf-8')
   return returned_string
