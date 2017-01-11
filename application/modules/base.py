9d957e645618188df7ccb72be782a330a9b2aab5
Fomin Alexey <fomin_ai@ekra.ru>
2017-01-11 14:10:53 +0300

    yjdfhnj

--- application/modules/base.py
+++ application/modules/base.py
@@ -33,14 +33,14 @@ def update_function(module, action, _id = None, data = None):
     # flash(u"хз чо происходит")
     # flash(u"%s"%str(module["actions"][action]["attrs"]))
     # return redirect(url_for('index'))
+    query = SET_QUERY%attrs
     try:
       for attr in module["actions"][action]["attrs"]: attrs.append(data[attr])
-      set_data_to_db(SET_QUERY%attrs)
+      set_data_to_db(query)
     except Exception as err:
       flash(u"Ошибка в процессе записи в базу данных новых значений")
       flash(str(err))
-      q = SET_QUERY%attrs
-      flash(u"%s"%q)
+      flash(u"%s"%query)
     else:
       flash(u"Данные успешно изменены")
     return redirect(url_for('index'))
