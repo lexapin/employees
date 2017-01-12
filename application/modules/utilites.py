61117be0bdda74ab8d318b0b1ac6da5c4d0291db
Fomin Alexey <fomin_ai@ekra.ru>
2017-01-12 18:21:29 +0300

    erg

--- application/modules/utilites.py
+++ application/modules/utilites.py
@@ -99,7 +99,7 @@ def create_input_field(item):
 import datetime
 def get_date(value):
   date = datetime.datetime.fromtimestamp(value)
-  return u"-".join([str(value) for value in [date.year, date.month, date.day]]
+  return u"-".join([str(value) for value in [date.year, date.month, date.day]])
 
 
 type_table = {
