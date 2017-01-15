358ac386ab58e454f7b5ee5af21eb634886b9f9b
Fomin Alexey <79625995110@ya.ru>
2017-01-15 18:13:31 +0400

    Signed-off-by: Fomin Alexey <79625995110@ya.ru>

--- application/modules/base.py
+++ application/modules/base.py
@@ -13,10 +13,10 @@ def table_view(module):
                   )
 
 
-def update_function(module, action, _id = None, data = None):
+def update_function(module, action, _id = None, request_data = None):
   GET_QUERY = module["actions"][action].get("get_query", None)
   SET_QUERY = module["actions"][action].get("set_query", None)
-  if data is None:
+  if request_data is None:
     db_items = None if _id is None else get_data_from_db(GET_QUERY%(_id,))[0]
     disabled = module["actions"][action].get("disabled", [])
     return render_template("form.html", 
@@ -26,27 +26,27 @@ def update_function(module, action, _id = None, data = None):
                           )
   else:
     if _id is not None:
-      if not get_data_from_db(GET_QUERY%(_id,)) or _id != data["_id"]:
+      if not get_data_from_db(GET_QUERY%(_id,)) or _id != request_data["_id"]:
         flash(u"Данные введены некорректно!!!")
         return redirect(url_for(module["base"]["name"]))
     try:
       encode_function = lambda attr: module["attributes"][attr].get("encode_function", lambda value: value)
-      data = set_data_to_db(SET_QUERY%tuple(encode_function(attr)(data[attr]) for attr in module["actions"][action]["attrs"]))
+      response_data = set_data_to_db(SET_QUERY%tuple(encode_function(attr)(request_data[attr]) for attr in module["actions"][action]["attrs"]))
     except Exception as err:
       flash(u"Ошибка в процессе записи в базу данных новых значений")
       flash(str(err))
       flash(u"%s"%SET_QUERY)
-      flash(u"%s"%str(tuple(data[attr] for attr in module["actions"][action]["attrs"])))
+      flash(u"%s"%str(tuple(request_data[attr] for attr in module["actions"][action]["attrs"])))
     else:
       flash(u"Данные успешно изменены")
-      flash(u"%s"%data)
+      flash(u"%s"%response_data)
       trigger_function = module["actions"][action].get("trigger", lambda value: None)
       trigger_attrs = module["actions"][action].get("vars_to_trigger", [])
-      data = [data]
+      data = [response_data]
       for attr in trigger_attrs:
         encode_function = lambda attr: module["attributes"][attr].get("encode_function", lambda value: value)
-        data.append(encode_function(attr)(data[attr]))
-      trigger_function(data)
+        response_data.append(encode_function(attr)(request_data[attr]))
+      trigger_function(response_data)
     return redirect(url_for(module["base"]["name"]))
 
 def delete_function(module, _id):
