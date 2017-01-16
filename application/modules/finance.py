8e35def2b5d8c4a15c00b76e6ff3c873a733f94b
Fomin Alexey <fomin_ai@ekra.ru>
2017-01-16 14:40:25 +0300

    finance готов
    Signed-off-by: Fomin Alexey <fomin_ai@ekra.ru>

--- application/modules/finance.py
+++ application/modules/finance.py
@@ -68,8 +68,8 @@ def add_employee_month_pay_association(data):
 finance_module = {
   "base": {
     "name": "finance",
-    "title": u"Бухгалтерская отчетность по зарплате",
-    "table_caption": u"Бухгалтерская отчетность по зарплате",
+    "title": u"Бухгалтерская ведомость по зарплате",
+    "table_caption": u"Бухгалтерская ведомость по зарплате",
     "form_caption": u"Информация о зарплате сотрудника",
     "function": table_view,
     "query": """
@@ -103,7 +103,7 @@ WHERE month_pay.id=month_pay_id AND month=%s AND year=%s AND employee_id=%s
       "disabled": ["_id"],
     },
     "edit": {
-      "caption": u"Обновить",
+      "caption": u"Редактировать",
       "function": update_function,
       "get_query": """
 SELECT month_pay.id, employee_id, month, year, salary, bonus
@@ -154,102 +154,3 @@ WHERE month_pay_id = %s AND month_pay.id=month_pay_id;
     },
   },
 }
-
-"""
-INSERT INTO month_pay (month, year, salary, bonus) VALUES (1, 2017, 10000, 5000);
-INSERT INTO employee_month_pay_association (employee_id, month_pay_id) VALUES (1, 1);
-INSERT INTO month_pay (month, year, salary, bonus) VALUES (2, 2017, 10000, 5000);
-INSERT INTO employee_month_pay_association (employee_id, month_pay_id) VALUES (1, 2);
-INSERT INTO month_pay (month, year, salary, bonus) VALUES (3, 2017, 10000, 5000);
-INSERT INTO employee_month_pay_association (employee_id, month_pay_id) VALUES (1, 3);
-INSERT INTO month_pay (month, year, salary, bonus) VALUES (1, 2017, 15000, 5000);
-INSERT INTO employee_month_pay_association (employee_id, month_pay_id) VALUES (2, 4);
-INSERT INTO month_pay (month, year, salary, bonus) VALUES (2, 2017, 15000, 5000);
-INSERT INTO employee_month_pay_association (employee_id, month_pay_id) VALUES (2, 5);
-INSERT INTO month_pay (month, year, salary, bonus) VALUES (3, 2017, 15000, 5000);
-INSERT INTO employee_month_pay_association (employee_id, month_pay_id) VALUES (2, 6);
-INSERT INTO month_pay (month, year, salary, bonus) VALUES (4, 2017, 15000, 4000);
-INSERT INTO employee_month_pay_association (employee_id, month_pay_id) VALUES (2, 4);
-INSERT INTO month_pay (month, year, salary, bonus) VALUES (4, 2017, 12000, 6000);
-INSERT INTO employee_month_pay_association (employee_id, month_pay_id) VALUES (1, 10);
-"""
-
-"""
-SELECT employee.id, first_name, last_name, month, year, salary, bonus
-FROM employee
-INNER JOIN employee_month_pay_association
-INNER JOIN month_pay 
-WHERE employee.id=employee_id AND month_pay.id=month_pay_id;
-"""
-
-"""
-SELECT employee.id, first_name, last_name, month, year, salary, bonus
-FROM employee
-INNER JOIN employee_month_pay_association
-INNER JOIN month_pay 
-WHERE employee.id=employee_id AND month_pay.id=month_pay_id
-AND month_pay_id IN (SELECT MAX(month_pay_id) FROM employee_month_pay_association GROUP BY employee_id);
-"""
-
-"""
-SELECT employee_id, first_name, last_name, SUM(salary), SUM(bonus)
-FROM employee
-INNER JOIN employee_month_pay_association
-INNER JOIN month_pay
-INNER JOIN (SELECT month, year FROM month_pay GROUP BY (year*100+month) ORDER BY (year*100+month) DESC LIMIT 2) d_m
-WHERE month_pay.id=month_pay_id AND month_pay.month=d_m.month AND month_pay.year=d_m.year AND employee_id = employee.id
-GROUP BY employee_id;
-"""
-
-"""
-SELECT month, year FROM month_pay
-GROUP BY (year*100+month)
-ORDER BY (year*100+month) DESC
-LIMIT 2;
-"""
-
-
-"""
-INSERT INTO month_pay (month, year, salary, bonus)
-SELECT * FROM (SELECT %(month)s, %(year)s, %(salary)s, %(bonus)s) AS tmp
-WHERE NOT EXISTS (
-SELECT employee_id, month, year
-FROM employee_month_pay_association
-INNER JOIN month_pay
-WHERE month_pay.id=month_pay_id AND month=%(month)s AND year=%(year)s
-) LIMIT 1;
-"""
-
-"""
-INSERT INTO month_pay (month, year, salary, bonus)
-SELECT * FROM (SELECT 12 as month, 2016 as year, 5000 as salary, 5000 as bonus) AS tmp
-WHERE NOT EXISTS (
-SELECT employee_id, month, year
-FROM employee_month_pay_association
-INNER JOIN month_pay
-WHERE month_pay.id=month_pay_id AND month=12 AND year=2016 AND employee_id=1
-) LIMIT 1;
-"""
-
-
-
-"""
-SELECT * 
-FROM month_pay
-ORDER BY (year*100+month) DESC
-LIMIT (SELECT COUNT(*) FROM employee WHERE id IN (SELECT employee_id FROM employee_month_pay_association GROUP BY employee_id));
-"""
-
-"""
-SELECT month_pay_id, employee_id, month, year
-FROM employee_month_pay_association
-INNER JOIN month_pay
-WHERE month_pay.id=month_pay_id
-GROUP BY (year*100+month)
-"""
-
-"""
-SELECT MAX(month_pay_id)
-FROM employee_month_pay_association
-GROUP BY employee_id;
-"""
