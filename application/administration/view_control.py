2385c930e4d06d7da32bf9d720f606f93581a0de
Fomin Alexey <fomin_ai@ekra.ru>
2017-01-17 18:38:51 +0300

    thetyjhteyj

--- application/administration/view_control.py
+++ application/administration/view_control.py
@@ -2,4 +2,27 @@
 from application.modules.utilites import *
 
 def registrate_view(module):
+  name = module["base"]["name"]
+  caption = module["base"]["title"]
+  load_view_query = "INSERT INTO view (name, caption)"
   return True
+
+place_module = {
+  "base": {
+    "name": "places",
+    "title": u"Должности сотрудников",
+    "table_caption": u"Должности сотрудников",
+    "form_caption": u"Информация о должности сотрудника",
+    "function": table_view,
+    "query": place_query,
+  },}
+
+"""
+INSERT INTO view (name, caption)
+SELECT * FROM (SELECT %s AS name, %s AS caption) AS tmp
+WHERE NOT EXISTS (
+SELECT name, caption
+FROM view
+WHERE view.name = tmp.name
+) LIMIT 1;
+"""
