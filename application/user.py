8b351bb370ed354f8e8579586f1b8243367d6d56
Fomin Alexey <fomin_ai@ekra.ru>
2017-01-18 10:53:20 +0300

    erhy

--- application/user.py
+++ application/user.py
@@ -28,7 +28,7 @@ class User(object):
     return unicode(self.id)
 
   def __repr__(self):
-    return self.username.encode("utf-8")
+    return self.username.decode("utf-8")
 
   @classmethod
   def get(cls, _id):
