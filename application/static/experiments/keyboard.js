a6103af1b87dd8c60f42cc48940307ec42d17de2
Aleksei Fomin <aleksei.fomin@arloid.com>
2017-05-17 23:44:29 +0300

    add key experiment functions

--- application/static/experiments/keyboard.js
+++ application/static/experiments/keyboard.js
@@ -2,6 +2,7 @@ $(function(){
   // keyboard form
   var key = false;
   var num = false;
+  var experiment_keys = null;
   $("#keyCheckbox").prop("checked", key);
   $("#numCheckbox").prop("checked", num);
   $("#keyboardModal").modal("show");
@@ -13,6 +14,14 @@ $(function(){
     }
     else {
       $("#keyboardModal").modal("hide");
+      experiment_keys = {};
+      if (key)
+        for (var key_char in char_keys)
+          experiment_keys[key_char] = char_keys[key_char];
+      if (num)
+        for (var key_char in num_keys)
+          experiment_keys[key_char] = num_keys[key_char];
+      start_experiment(_.keys(experiment_keys))
     }
   });
   // keyboard codes
@@ -53,5 +62,52 @@ $(function(){
       console.log(path_id);
       Snap(path_id).attr({fill:DEFAULT_FILL})
     }
+  }
+  // experiment vars
+  var max_experiments = 10;
+  var count = 0;
+  var CURRENT_RANDOM_CHAR = null;
+  var experiments = {};
+  // experiment functions
+  var get_old_fill = function(snap_object){
+    return DEFAULT_FILL
+  }
+  var keyDownTextField = function(e) {
+    console.log("dshdfshn");
+    var keyCode = e.keyCode;
+    $("#key").html(String.fromCharCode(event.which));
+    console.log(event.keyCode, String.fromCharCode(event.which));
+    if (CURRENT_RANDOM_CHAR == keyCode) {close_experiment();}
+    console.log("ВЫШЕЛ");
+  }
+
+  document.addEventListener("keyup", keyDownTextField, false);
+
+  var randomChar = function(chars) {
+    max = chars.length
+    var rand = Math.random() * (max);
+    rand = Math.floor(rand);
+    return chars[rand];
+  }
+
+  var start_experiment = function(chars){
+    var char = randomChar(chars);
+    var char_id = experiment_keys[char];
+    var snap_object = Snap(char_id)
+    DEFAULT_FILL = get_old_fill(snap_object)
+    snap_object.attr({ fill: "#ff0000" });
+    CURRENT_RANDOM_CHAR = char;
+    experiments[count] = Date.now();
+  }
+
+  var close_experiment = function(){
+    experiments[count] = Date.now() - experiments[count];
+    count++;
+    if (count<10){
+      setTimeout(start_experiment, 500, _.keys(experiment_keys));
+    }
+    var char_id = experiment_keys[CURRENT_RANDOM_CHAR];
+    var snap_object = Snap(char_id);
+    snap_object.attr({ fill: DEFAULT_FILL });
   }
 });
