ee911d3421647205345d5cdbb603fb3042f0958f
Aleksei Fomin <aleksei.fomin@arloid.com>
2017-05-17 18:16:57 +0300

    test modal hide

--- application/static/experiments/keyboard.js
+++ application/static/experiments/keyboard.js
@@ -4,5 +4,15 @@ $(function(){
   var num = false;
   $("#keyCheckbox").prop("checked", key);
   $("#numCheckbox").prop("checked", num);
-  $("#keyboardModal").modal();
+  $("#keyboardModal").modal("show");
+  $(".btn-primary").click(function(){
+    key = $("#keyCheckbox").prop("checked");
+    num = $("#numCheckbox").prop("checked");
+    if (!key && !num) {
+      $("#keyboardModal").modal("hide");
+    }
+    else {
+      alert("Выберите для теста хотя бы одну клавиатуру");
+    }
+  });
 });
