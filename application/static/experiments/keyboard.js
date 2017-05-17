02b931e2fa72b0312f86c7fa2f66c887c8845c1b
Aleksei Fomin <aleksei.fomin@arloid.com>
2017-05-17 18:30:42 +0300

    test load svg querty

--- application/static/experiments/keyboard.js
+++ application/static/experiments/keyboard.js
@@ -10,10 +10,21 @@ $(function(){
     num = $("#numCheckbox").prop("checked");
     if (!key && !num) {
       alert("Выберите для теста хотя бы одну клавиатуру");
-
     }
     else {
       $("#keyboardModal").modal("hide");
     }
   });
+  // svg
+  var DEFAULT_FILL = "#97c5d5";
+  var KEYDOWN_FILL = "#ff0000";
+  var s = Snap("#svg");
+  Snap.load("https://upload.wikimedia.org/wikipedia/commons/3/3a/Qwerty.svg", onSVGLoaded );
+  function onSVGLoaded( data ){ 
+    s.append( data );
+    for (var key in keys) {
+      var path_id = keys[key];
+      Snap(path_id).attr({fill:DEFAULT_FILL})
+    }
+  }
 });
