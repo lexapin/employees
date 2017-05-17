3adf37ab14fb9030a8cee62bd0a7cb10218b0583
Aleksei Fomin <aleksei.fomin@arloid.com>
2017-05-17 18:44:08 +0300

    solved object error

--- application/static/experiments/keyboard.js
+++ application/static/experiments/keyboard.js
@@ -40,7 +40,7 @@ $(function(){
     105: "#path167",
     96:  "#path373",
   };
-  var keys = char_keys + num_keys;
+  var keys = Object.assign(char_keys, num_keys);
   // svg
   var DEFAULT_FILL = "#97c5d5";
   var KEYDOWN_FILL = "#ff0000";
