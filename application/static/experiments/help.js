6068511d6e0c6d3abf17ea50f3abe6c7f2f30121
Aleksei Fomin <aleksei.fomin@arloid.com>
2017-05-30 17:29:44 +0300

    werg

--- application/static/experiments/help.js
+++ application/static/experiments/help.js
@@ -1,7 +1,7 @@
 $(function() {
   $(".menu").click(function(){
-    // var pathname = window.location.pathname; // Returns path only
-    // var url      = window.location.href;
+    console.log(window.location.pathname); // Returns path only
+    console.log(window.location.href);
     window.location.replace(window.location.href+"/"+$(this).attr("id"));
   })
 })
