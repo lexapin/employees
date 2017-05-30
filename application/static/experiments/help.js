a05f043c0f50c31832eaa76f743a41e54839c4f2
Aleksei Fomin <aleksei.fomin@arloid.com>
2017-05-30 17:36:52 +0300

    dsfvg

--- application/static/experiments/help.js
+++ application/static/experiments/help.js
@@ -2,7 +2,7 @@ $(function() {
   $(".menu").click(function(){
     console.log(window.location.pathname); // Returns path only
     console.log(window.location.href);
-    console.log(window.location.hostname);
-    window.location.replace(window.location.hostname+"help/"+$(this).attr("id"));
+    console.log(document.domain);
+    window.location.replace(document.domain+"help/"+$(this).attr("id"));
   })
 })
