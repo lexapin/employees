53f8d871a1de8931d17799e2495e30a9af60aee6
Aleksei Fomin <aleksei.fomin@arloid.com>
2017-05-30 17:34:12 +0300

    sdfsdg

--- application/static/experiments/help.js
+++ application/static/experiments/help.js
@@ -2,6 +2,7 @@ $(function() {
   $(".menu").click(function(){
     console.log(window.location.pathname); // Returns path only
     console.log(window.location.href);
-    window.location.replace(window.location.pathname+"/"+$(this).attr("id"));
+    console.log(window.location.hostname);
+    window.location.replace(window.location.hostname+"help/"+$(this).attr("id"));
   })
 })
