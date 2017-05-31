f988f45ae42e1b4fc8fb73cd8c79e8ad99b91ae8
Aleksei Fomin <aleksei.fomin@arloid.com>
2017-05-31 12:09:14 +0300

    add links

--- application/static/experiments/help.js
+++ application/static/experiments/help.js
@@ -1,8 +1,8 @@
 $(function() {
   $(".menu").click(function(){
-    console.log(window.location.pathname); // Returns path only
-    console.log(window.location.href);
-    console.log(document.domain);
     window.location.replace("http://"+document.domain+"/help/"+$(this).attr("id"));
-  })
+  });
+  $("p.class").click(function(){
+    window.location.replace("http://"+document.domain+"/help/"+$(this).attr("id"));
+  });
 })
