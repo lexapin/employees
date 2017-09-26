433988c72bbb95bfb270dc9412478333b426f1b7
Aleksei <aleksei.fomin@arloid.com>
2017-09-26 16:54:50 +0300

    resolve some decoration view

--- application/static/experiments/possibility.js
+++ application/static/experiments/possibility.js
@@ -15,7 +15,10 @@ $(function(){
     var z=-3.74-0.14*arr[0]-0.36*arr[1]+0.16*arr[2]+0.69*arr[3]+0.02*arr[4]-1.03*arr[5]+0.79*arr[6];
     console.log(z);
     var p = 1.0/(1.0+Math.exp(-z))*100;
-    $("#result").append("<h3>"+p.toFixed(2)+"%"+"</h3>");
+    var description;
+    if (p>20) description = "Высокий риск несостоятельности швов колоректального рака";
+    else description = "Низкий риск развития несостоятельности швов колоректального анастомоза";
+    $("#result").append("<h3>"+p.toFixed(2)+"%"+"<br>"+description+"</h3>);
     $("#reportModal").modal("show");
   });
 })
